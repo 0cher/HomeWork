a68bafe28556823b9023166d54a111af041740dc
0cher <aleksejs.z@icloud.com>
2022-11-13 23:56:50 +0200

    HomeWork 8

--- src/HomeWork_8/Exception/Main.java
+++ src/HomeWork_8/Exception/Main.java
@@ -17,7 +17,7 @@ public class Main {
             System.err.println(e.getMessage());
             throw new RuntimeException("error",e);
         }
-        System.out.println("Мы Вас приветствуем "+person.getNameSurname());
+        System.out.println("Мы Вас приветствуем "+person.getName()+" "+person.getSurname());
         System.out.println("Ваш возраст: " +person.getAge());
     }
 }
--- src/HomeWork_8/Exception/Person.java
+++ src/HomeWork_8/Exception/Person.java
@@ -5,12 +5,16 @@ import java.util.regex.Pattern;
 
 public class Person {
     private int age;
-    private String nameSurname;
+    private String name;
+    private String surname;
     public int getAge() {
         return age;
     }
-    public String getNameSurname() {
-        return nameSurname;
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
     }
     public void setAgeNameSurname (String ageNameSurname) throws IncorrectInfoException{
         String patternNameSurname = "([\\wа-яА-Я]{0,100}),?\s([\\wа-яА-Я]{0,100}),?\s.+";
@@ -26,8 +30,9 @@ public class Person {
             throw new IncorrectInfoException("не корректный возраст");
         }
         if (matcherNS.matches()&&matcherA.matches()){
-            this.nameSurname = matcherNS.group(1).substring(0,1).toUpperCase()+
-            matcherNS.group(1).substring(1)+" "+matcherNS.group(2).substring(0,1).toUpperCase()+
+            this.name = matcherNS.group(1).substring(0,1).toUpperCase()+
+                    matcherNS.group(1).substring(1);
+            this.surname =matcherNS.group(2).substring(0,1).toUpperCase()+
                     matcherNS.group(2).substring(1);
             this.age = Integer.parseInt(matcherA.group(1));
         }
