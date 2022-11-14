e2f940666a9626dde613e7a1bcf3f1ebbdf13eab
0cher <aleksejs.z@icloud.com>
2022-11-14 11:40:28 +0200

    HomeWork 8

--- src/HomeWork_8/Exception/Person.java
+++ src/HomeWork_8/Exception/Person.java
@@ -2,7 +2,6 @@ package HomeWork_8.Exception;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class Person {
     private int age;
     private String name;
@@ -21,13 +20,16 @@ public class Person {
         Pattern patternNS = Pattern.compile(patternNameSurname);
         Matcher matcherNS = patternNS.matcher(ageNameSurname);
         if (!matcherNS.matches()){
-            throw new IncorrectInfoException("Имя или фамилия введены не корректно");
+            throw new IncorrectInfoException("Имя или фамилия введены не корректно: "+
+                    ageNameSurname);
         }
         String patternAge = ".*\s([0-9][0-9]?)";
         Pattern patternA = Pattern.compile(patternAge);
         Matcher matcherA = patternA.matcher(ageNameSurname);
         if (!matcherA.matches()){
-            throw new IncorrectInfoException("не корректный возраст");
+            throw new IncorrectInfoException("Не корректный возраст: "+
+                    ageNameSurname.replaceAll(
+                            "([\\wа-яА-Я]{0,100}),?\s([\\wа-яА-Я]{0,100}),?\s",""));
         }
         if (matcherNS.matches()&&matcherA.matches()){
             this.name = matcherNS.group(1).substring(0,1).toUpperCase()+
